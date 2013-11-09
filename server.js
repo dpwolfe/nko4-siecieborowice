770d8ac6b1bf665859368e7f2341bff09b5c112b
Kevin Burns <burnske6@gmail.com>
2013-11-08 17:13:45 -0800

    Adding AuthO Setup File
    Initialize AuthO Setup.  Next Steps Initialize Passport and setup
    callback

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('yTltgBB-j8Iq2LKk');
 
+var passport = require('passport');
+var strategy = require('./lib/setup-passport');
+
 var isProduction = (process.env.NODE_ENV === 'production');
 var http = require('http');
 var port = (isProduction ? 80 : 8000);
