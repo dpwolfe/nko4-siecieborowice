c05c17a56a27ca8aa71f42269d6eb131a904906c
David Wolfe <david.p.wolfe@gmail.com>
2013-11-08 20:06:16 -0800

    Moving files.  Updating compiled js files.

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ var Greeter = (function () {
             return _this.span.innerHTML = new Date().toUTCString();
         }, 500);
     };
+
     Greeter.prototype.stop = function () {
         clearTimeout(this.timerToken);
     };
     return Greeter;
 })();
+
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
 };
-//@ sourceMappingURL=app.js.map
+//# sourceMappingURL=app.js.map
