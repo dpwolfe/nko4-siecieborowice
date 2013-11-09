ce2b058b5938ee40289f790f3a753f5594471e18
Kevin Burns <burnske6@gmail.com>
2013-11-08 19:15:35 -0800

    Adding Type Definitions to csproj

--- siecieborowice/server.js
+++ siecieborowice/server.js
@@ -1,34 +1,28 @@
-// https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('yTltgBB-j8Iq2LKk');
-
 var passport = require('passport');
 var strategy = require('./lib/setup-passport');
-
 var isProduction = (process.env.NODE_ENV === 'production');
 var http = require('http');
 var port = (isProduction ? 80 : 8000);
-
 http.createServer(function (req, res) {
-    // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
     var voteko = '<iframe src="http://nodeknockout.com/iframe/siecieborowice" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
     res.end('<html><body>' + voteko + '</body></html>\n');
 }).listen(port, function (err) {
-    if (err) {
+    if(err) {
         console.error(err);
         process.exit(-1);
     }
-
-    if (process.getuid() === 0) {
+    if(process.getuid() === 0) {
         require('fs').stat(__filename, function (err, stats) {
-            if (err) {
+            if(err) {
                 return console.error(err);
             }
             process.setuid(stats.uid);
         });
     }
-
     console.log('Server running at http://0.0.0.0:' + port + '/');
 });
-//# sourceMappingURL=server.js.map
+//@ sourceMappingURL=server.js.map
